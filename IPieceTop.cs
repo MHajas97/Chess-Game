fb781c0810bef5a7970ddb334103fad0048b66aa
Martin Hajas <martin.hajas97@gmail.com>
2020-09-15 11:51:20 +0300

    Transfered from GitLab

---
+++ IPieceTop.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace SharpChess
+{
+	public interface IPieceTop
+	{
+		Piece Base
+		{
+			get;
+		}
+
+		string Abbreviation
+		{
+			get;
+		}
+
+		Piece.enmName Name
+		{
+			get;
+		}
+	
+		int BasicValue
+		{
+			get;
+		}
+
+		int Value
+		{
+			get;
+		}
+
+		int PositionalValue
+		{
+			get;
+		}
+
+		int ImageIndex
+		{
+			get;
+		}
+	
+		bool CanBeTaken
+		{
+			get;
+		}
+
+
+		void GenerateLazyMoves(Moves moves, Moves.enmMovesType movesType);
+	}
+}
